9ab6e90391b7ca0fc524f8c5c083f53ff6cfb8ac
Balazs Sebesteny <sebestenybalazs@gmail.com>
2024-07-20 00:49:31 +1200

    Fix config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,13 @@
 export default defineNuxtConfig({
-  extends: 'content-wind',
-  routeRules: {
-    '/': { prerender: true }
-  },
-  content: {
-      theme: {
-        default: 'github-light',
-        dark: 'github-dark',
-        sepia: 'monokai'
-      }
+    extends: 'content-wind',
+    routeRules: {
+        '/': { prerender: true }
+    },
+    content: {
+        theme: {
+            default: 'github-light',
+            dark: 'github-dark',
+            sepia: 'monokai'
+        }
     }
-  }
 })
